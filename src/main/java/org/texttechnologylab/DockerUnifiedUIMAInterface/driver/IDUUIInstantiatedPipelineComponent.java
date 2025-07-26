8a1106e48da8a94204ecb24e84e5c029123eb519
dterefe <daveterefe@mail.com>
2025-07-26 15:54:09 +0200

    Revert "Synchronized Component requests"
    This reverts commit 63fb09c5cece5da69228a032b5020a869365541d.

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/driver/IDUUIInstantiatedPipelineComponent.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/driver/IDUUIInstantiatedPipelineComponent.java
@@ -57,56 +57,42 @@ public interface IDUUIInstantiatedPipelineComponent {
      * @throws ResourceInitializationException
      */
     public static TypeSystemDescription getTypesystem(String uuid, IDUUIInstantiatedPipelineComponent comp) throws ResourceInitializationException {
-        Triplet<IDUUIUrlAccessible, Long, Long> queue = comp.getComponent();
+        Triplet<IDUUIUrlAccessible,Long,Long> queue = comp.getComponent();
+        //System.out.printf("Address %s\n",queue.getValue0().generateURL()+ DUUIComposer.V1_COMPONENT_ENDPOINT_TYPESYSTEM);
+
         int tries = 0;
-
-        String url = queue.getValue0().generateURL() + DUUIComposer.V1_COMPONENT_ENDPOINT_TYPESYSTEM;
-        System.out.printf("[DEBUG][%s] getTypesystem: URL=%s%n", uuid, url);
-
-        while (tries < 100) {
+        while(tries < 100) {
             tries++;
             try {
-                System.out.printf("[DEBUG][%s] Attempt #%d: Creating HTTP GET request to %s%n", uuid, tries, url);
-
                 HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create(url))
+                        .uri(URI.create(queue.getValue0().generateURL() + DUUIComposer.V1_COMPONENT_ENDPOINT_TYPESYSTEM))
                         .version(HttpClient.Version.HTTP_1_1)
                         .GET()
                         .build();
-
-                System.out.printf("[DEBUG][%s] Sending request...%n", uuid);
-                HttpResponse<byte[]> resp = _client.send(request, HttpResponse.BodyHandlers.ofByteArray());
-                System.out.printf("[DEBUG][%s] Got response: Status=%d, BodySize=%d%n", uuid, resp.statusCode(), resp.body() != null ? resp.body().length : -1);
-
+                HttpResponse<byte[]> resp = _client.sendAsync(request, HttpResponse.BodyHandlers.ofByteArray()).join();
                 if (resp.statusCode() == 200) {
                     String body = new String(resp.body(), Charset.defaultCharset());
                     File tmp = File.createTempFile("duui.composer", "_type");
                     tmp.deleteOnExit();
-                    System.out.printf("[DEBUG][%s] Writing typesystem to temp file: %s, Size=%d%n", uuid, tmp.getAbsolutePath(), body.length());
                     FileWriter writer = new FileWriter(tmp);
                     writer.write(body);
                     writer.flush();
                     writer.close();
-
                     comp.addComponent(queue.getValue0());
-                    System.out.printf("[DEBUG][%s] TypeSystemDescriptionFactory.createTypeSystemDescriptionFromPath(%s)%n", uuid, tmp.toURI().toString());
+
+
                     return TypeSystemDescriptionFactory.createTypeSystemDescriptionFromPath(tmp.toURI().toString());
                 } else {
                     comp.addComponent(queue.getValue0());
-                    System.out.printf("[%s][DEBUG] Endpoint did not provide typesystem (Status %d), using default one...\n", uuid, resp.statusCode());
+                    System.out.printf("[%s]: Endpoint did not provide typesystem, using default one...\n", uuid);
                     return TypeSystemDescriptionFactory.createTypeSystemDescription();
                 }
             } catch (Exception e) {
-                System.out.printf("[DEBUG][%s] Exception on try %d/%d: %s (%s)%n", uuid, tries, 100, e.getClass().getSimpleName(), e.getMessage());
-                e.printStackTrace(System.out);
-                System.out.printf("Cannot reach endpoint, trying again %d/%d...\n", tries + 1, 100);
-                try { Thread.sleep(250); } catch (InterruptedException ignored) {}
+                System.out.printf("Cannot reach endpoint trying again %d/%d...\n",tries+1,100);
             }
         }
-        System.out.printf("[DEBUG][%s] All tries failed, endpoint unreachable!%n", uuid);
         throw new ResourceInitializationException(new Exception("Endpoint is unreachable!"));
     }
-
 
     /**
      * Calling the DUUI component
@@ -117,10 +103,7 @@ public interface IDUUIInstantiatedPipelineComponent {
      * @throws PipelineComponentException
      */
     public static void process(JCas jc, IDUUIInstantiatedPipelineComponent comp, DUUIPipelineDocumentPerformance perf) throws CASException, PipelineComponentException {
-        Triplet<IDUUIUrlAccessible, Long, Long> queue = comp.getComponent();
-        String uuid = java.util.UUID.randomUUID().toString(); // Für Tracing, falls kein besseres vorhanden
-
-        System.out.printf("[DEBUG][%s] process(): Start for component %s%n", uuid, comp.getPipelineComponent().getName());
+        Triplet<IDUUIUrlAccessible,Long,Long> queue = comp.getComponent();
 
         IDUUICommunicationLayer layer = queue.getValue0().getCommunicationLayer();
         long serializeStart = System.nanoTime();
@@ -131,44 +114,38 @@ public interface IDUUIInstantiatedPipelineComponent {
             JCas viewJc;
             if(viewName == null) {
                 viewJc = jc;
-                System.out.printf("[DEBUG][%s] No specific view requested, using main JCas%n", uuid);
-            } else {
+            }
+            else {
                 try {
                     viewJc = jc.getView(viewName);
-                    System.out.printf("[DEBUG][%s] Using view: %s%n", uuid, viewName);
                 }
                 catch(CASException e) {
                     if(pipelineComponent.getCreateViewFromInitialView()) {
                         viewJc = jc.createView(viewName);
                         viewJc.setDocumentText(jc.getDocumentText());
                         viewJc.setDocumentLanguage(jc.getDocumentLanguage());
-                        System.out.printf("[DEBUG][%s] View %s not found, created new with copied text/lang.%n", uuid, viewName);
-                    } else {
-                        System.out.printf("[DEBUG][%s] View %s not found, and not allowed to create.%n", uuid, viewName);
+                    }
+                    else {
                         throw e;
                     }
                 }
             }
 
             if (layer.supportsProcess()) {
-                System.out.printf("[DEBUG][%s] Using optimized communication layer for process()%n", uuid);
                 JCas sourceCas = viewJc.getView(comp.getSourceView());
                 JCas targetCas;
                 try {
                     targetCas = viewJc.getView(comp.getTargetView());
                 } catch (CASException e) {
                     targetCas = viewJc.createView(comp.getTargetView());
-                    System.out.printf("[DEBUG][%s] Target view %s not found, created new.%n", uuid, comp.getTargetView());
                 }
 
                 layer.process(
-                    sourceCas,
-                    new DUUIHttpRequestHandler(_client, queue.getValue0().generateURL(), pipelineComponent.getTimeout()),
-                    comp.getParameters(),
-                    targetCas
+                        sourceCas,
+                        new DUUIHttpRequestHandler(_client, queue.getValue0().generateURL(), pipelineComponent.getTimeout()),
+                        comp.getParameters(),
+                        targetCas
                 );
-
-                System.out.printf("[DEBUG][%s] Layer.process() done, writing annotation...%n", uuid);
 
                 ReproducibleAnnotation ann = new ReproducibleAnnotation(jc);
                 ann.setDescription(comp.getPipelineComponent().getFinalizedRepresentation());
@@ -183,50 +160,44 @@ public interface IDUUIInstantiatedPipelineComponent {
             ByteArrayOutputStream out = new ByteArrayOutputStream(1024*1024);
 
             // Invoke Lua serialize()
-            System.out.printf("[DEBUG][%s] Calling layer.serialize()...%n", uuid);
             layer.serialize(viewJc, out, comp.getParameters(), comp.getSourceView());
+
 
             byte[] ok = out.toByteArray();
             long sizeArray = ok.length;
             long serializeEnd = System.nanoTime();
-            System.out.printf("[DEBUG][%s] Serialize finished. Size: %d bytes, Time: %d ms%n", uuid, sizeArray, (serializeEnd-serializeStart)/1_000_000);
 
             long annotatorStart = serializeEnd;
             int tries = 0;
             HttpResponse<byte[]> resp = null;
-            String processUrl = queue.getValue0().generateURL() + DUUIComposer.V1_COMPONENT_ENDPOINT_PROCESS;
-            System.out.printf("[DEBUG][%s] Target endpoint: %s%n", uuid, processUrl);
             while (tries < 3) {
                 tries++;
                 try {
                     HttpRequest request = HttpRequest.newBuilder()
-                            .uri(URI.create(processUrl))
-                            .timeout(Duration.ofSeconds(pipelineComponent.getTimeout()))
+                            .uri(URI.create(queue.getValue0().generateURL() + DUUIComposer.V1_COMPONENT_ENDPOINT_PROCESS))
+                            .timeout(Duration.ofSeconds(comp.getPipelineComponent().getTimeout()))
                             .POST(HttpRequest.BodyPublishers.ofByteArray(ok))
                             .version(HttpClient.Version.HTTP_1_1)
                             .build();
-                    System.out.printf("[DEBUG][%s] Attempt #%d: Sending POST (size: %d bytes)%n", uuid, tries, ok.length);
-                    resp = _client.send(request, HttpResponse.BodyHandlers.ofByteArray());
-                    System.out.printf("[DEBUG][%s] Response: Status=%d, Size=%d bytes%n", uuid, resp.statusCode(), resp.body()!=null?resp.body().length:-1);
+                    resp = _client.sendAsync(request, HttpResponse.BodyHandlers.ofByteArray()).join();
                     break;
                 } catch (Exception e) {
-                    System.out.printf("[DEBUG][%s] Exception on try %d/%d: %s (%s)%n", uuid, tries, 3, e.getClass().getSimpleName(), e.getMessage());
-                    e.printStackTrace(System.out);
-                    try { Thread.sleep(500); } catch (InterruptedException ignored) {}
+                    e.printStackTrace();
+                    //System.out.printf("Cannot reach endpoint trying again %d/%d...\n",tries+1,10);
                 }
             }
             if (resp == null) {
-                System.out.printf("[DEBUG][%s] Could not reach endpoint after 3 tries!%n", uuid);
                 throw new IOException("Could not reach endpoint after 3 tries!");
             }
+
 
             if (resp.statusCode() == 200) {
                 ByteArrayInputStream st = new ByteArrayInputStream(resp.body());
                 long annotatorEnd = System.nanoTime();
                 long deserializeStart = annotatorEnd;
 
-                System.out.printf("[DEBUG][%s] Deserializing response into layer...%n", uuid);
                 layer.deserialize(viewJc, st, comp.getTargetView());
+
                 long deserializeEnd = System.nanoTime();
 
                 ReproducibleAnnotation ann = new ReproducibleAnnotation(jc);
@@ -237,14 +208,10 @@ public interface IDUUIInstantiatedPipelineComponent {
                 ann.addToIndexes();
                 perf.addData(serializeEnd - serializeStart, deserializeEnd - deserializeStart, annotatorEnd - annotatorStart, queue.getValue2() - queue.getValue1(), deserializeEnd - queue.getValue1(), String.valueOf(comp.getPipelineComponent().getFinalizedRepresentationHash()), sizeArray, jc, null);
 
-                System.out.printf("[DEBUG][%s] Deserialization done, annotation added. Timing (ms): serialize=%d, deserialize=%d, annotator=%d%n", uuid, (serializeEnd-serializeStart)/1_000_000, (deserializeEnd-deserializeStart)/1_000_000, (annotatorEnd-annotatorStart)/1_000_000);
-
             } else {
                 ByteArrayInputStream st = new ByteArrayInputStream(resp.body());
                 String responseBody = new String(st.readAllBytes(), StandardCharsets.UTF_8);
                 st.close();
-
-                System.out.printf("[DEBUG][%s] Non-200 response: %d %s%n", uuid, resp.statusCode(), responseBody);
 
                 // track "performance" of error documents if not explicitly disabled
                 if (perf.shouldTrackErrorDocs()) {
@@ -264,10 +231,8 @@ public interface IDUUIInstantiatedPipelineComponent {
                 }
             }
         } catch (CASException e) {
-            System.out.printf("[DEBUG][%s] CASException: %s%n", uuid, e.getMessage());
             throw e;
         } catch (Exception e) {
-            System.out.printf("[DEBUG][%s] Exception: %s%n", uuid, e.getMessage());
             try {
                 DocumentMetaData documentMetaData = DocumentMetaData.get(jc);
                 throw new PipelineComponentException(comp.getPipelineComponent(), documentMetaData, e);
@@ -275,11 +240,9 @@ public interface IDUUIInstantiatedPipelineComponent {
                 throw new PipelineComponentException(comp.getPipelineComponent(), e);
             }
         } finally {
-            System.out.printf("[DEBUG][%s] Adding component back to pool.%n", uuid);
             comp.addComponent(queue.getValue0());
         }
     }
-
 
     /**
      * @deprecated
